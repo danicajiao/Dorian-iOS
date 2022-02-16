23602cbd74b4c3307b4dbda748ccf77750e928d1
danicajiao <cajiao.dani@gmail.com>
2022-02-16 13:48:03 -0500

    Initial Commit

---
+++ CoffeeCommerce/CoffeeCommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeCommerceApp.swift
+//  CoffeeCommerce
+//
+//  Created by Daniel Cajiao on 2/16/22.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeCommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
